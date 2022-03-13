d9593e1c2bc1efd45cffdb9ac21969e603dff5fa
chinmayee <chinmayeedalaibharati@gmail.com>
2022-03-13 22:40:43 +0530

    add files

---
+++ chapter_2.py
@@ -0,0 +1,232 @@
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+class AdalineGD:
+    """ADAptive LInear NEuron classifier.
+
+    Parameters
+    ------------
+    eta : float
+      Learning rate (between 0.0 and 1.0)
+    n_iter : int
+      Passes over the training dataset.
+    random_state : int
+      Random number generator seed for random weight
+      initialization.
+
+
+    Attributes
+    -----------
+    w_ : 1d-array
+      Weights after fitting.
+    b_ : Scalar
+      Bias unit after fitting.
+    losses_ : list
+      Mean squared eror loss function values in each epoch.
+
+    """
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+          Training vectors, where n_examples is the number of examples and
+          n_features is the number of features.
+        y : array-like, shape = [n_examples]
+          Target values.
+
+        Returns
+        -------
+        self : object
+
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1])
+        self.b_ = np.float_(0.)
+        self.losses_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            # Please note that the "activation" method has no effect
+            # in the code since it is simply an identity function. We
+            # could write `output = self.net_input(X)` directly instead.
+            # The purpose of the activation is more conceptual, i.e.,  
+            # in the case of logistic regression (as we will see later), 
+            # we could change it to
+            # a sigmoid function to implement a logistic regression classifier.
+            output = self.activation(net_input)
+            errors = (y - output)
+            
+            #for w_j in range(self.w_.shape[0]):
+            #    self.w_[w_j] += self.eta * (2.0 * (X[:, w_j]*errors)).mean()
+            
+            self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0]
+            self.b_ += self.eta * 2.0 * errors.mean()
+            loss = (errors**2).mean()
+            self.losses_.append(loss)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_) + self.b_
+
+    def activation(self, X):
+        """Compute linear activation"""
+        return X
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+
+
+from matplotlib.colors import ListedColormap
+
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('o', 's', '^', 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class examples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    c=colors[idx],
+                    marker=markers[idx], 
+                    label=f'Class {cl}', 
+                    edgecolor='black')
+
+
+
+class AdalineSGD:
+    
+    
+    """ADAptive LInear NEuron classifier.
+
+    Parameters
+    ------------
+    eta : float
+      Learning rate (between 0.0 and 1.0)
+    n_iter : int
+      Passes over the training dataset.
+    shuffle : bool (default: True)
+      Shuffles training data every epoch if True to prevent cycles.
+    random_state : int
+      Random number generator seed for random weight
+      initialization.
+
+
+    Attributes
+    -----------
+    w_ : 1d-array
+      Weights after fitting.
+    b_ : Scalar
+        Bias unit after fitting.
+    losses_ : list
+      Mean squared error loss function value averaged over all
+      training examples in each epoch."""
+
+        
+    def __init__(self, n_iter = 10, eta = 0.01,random_state = None, shuffle = True):
+        
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.random_state = random_state
+        self.shuffle = shuffle
+    def fit(self, X, y):
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_examples, n_features]
+          Training vectors, where n_examples is the number of examples and
+          n_features is the number of features.
+        y : array-like, shape = [n_examples]
+          Target values.
+
+        Returns
+        -------
+        self : object
+
+        """
+        sel.initilize_weights(X.shape[1])
+        self.losses_ = []
+        for i in range(self.n_iter):
+            if self.shuffle:                
+                X,y = self._shuffle(X,y)
+            losses = []
+            for xi, target in zip(X, y):
+                losses.append(self._update_weights(xi, target))
+            avg_loss = np.mean(losses)
+            self.lossses_.append(avg_loss)
+        return self
+#The self parameter is a reference to the current instance of the class, and is used to access variables that belongs to the class.    
+    def partial_fit(self, X, y):
+        """Fit training data without reinitializing the weights"""
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X,y):
+                self._update_weights(xi,target)
+        else:
+            self._update_weights(X,y)
+        return self
+    
+    def _shuffle(self, X, y):
+        """ Shuffle training data"""
+        r = self.rgen.permutation(len(y))
+        return X[r], y[r]
+    
+    def _initialize_weights(self, m):
+        """ Initialize weights to small random numbers"""
+        self.rgen = np.random.RandomState(loc = 0.0, scale = 0.01, size = m)
+        self.w_ = self.rgen.normal(loc = 0.0, scale = 0.01, size = m)
+        self.b_ = np.float_(0.)
+        self.w_initialized = True
+        
+        
+    def _update_weights(self, xi, target):
+        """ Apply Adaline learning rule to update the weights"""
+        output  = self.activation(self.net_input(xi))
+        error = (target-output)
+        self.w_ += self.eta * 2.0 * xi * (error)
+        self.b_ += self.eta * 2.0 * error
+        loss = error ** 2
+        return loss
+    
+    def net_input(self, X):
+        """ Calculate net input"""
+        return np.dot(X, self.w_) + self.b_
+    
+    def activation(self, X):
+        """ Compute linear activation"""
+        return X
+    
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+        
+    
+    
